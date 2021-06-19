a7ba2545e8138ade14f189a52d86675d1f91cb0d
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-06-19 20:04:38 +0100

    Added button to enter & exit full screen mode

--- src/fullScreen.js
+++ src/fullScreen.js
@@ -1,8 +1,8 @@
 /* fullScreen.js */
-console.log("fullScreen.js is Here!");
+// console.log("fullScreen.js is Here!");
 
 let stage = document.getElementById("stage");
-
+/*
 stage.addEventListener(
   "click",
   (event) => {
@@ -10,3 +10,16 @@ stage.addEventListener(
   },
   false
 );
+*/
+
+let screenSizeBtn = document.getElementById("minMaxBtn");
+screenSizeBtn.addEventListener("click", (event) => {
+  // check if any element is full screen
+  if (!document.fullscreenElement) {
+    // if not make the stage full screen
+    stage.requestFullscreen();
+  } else {
+    //If there is, exit full screen
+    document.exitFullscreen();
+  }
+});
