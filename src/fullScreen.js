a1957dd7da89920ce4dcc8b8ca2cf80febe9cfad
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-06-19 20:43:20 +0100

    updated fullscreen.js

--- src/fullScreen.js
+++ src/fullScreen.js
@@ -13,13 +13,17 @@ stage.addEventListener(
 */
 
 let screenSizeBtn = document.getElementById("minMaxBtn");
-screenSizeBtn.addEventListener("click", (event) => {
-  // check if any element is full screen
-  if (!document.fullscreenElement) {
-    // if not make the stage full screen
-    stage.requestFullscreen();
-  } else {
-    //If there is, exit full screen
-    document.exitFullscreen();
-  }
-});
+screenSizeBtn.addEventListener(
+  "click",
+  (event) => {
+    // check if any element is full screen
+    if (!document.fullscreenElement) {
+      // if not make the stage full screen
+      stage.requestFullscreen();
+    } else {
+      //If there is, exit full screen
+      document.exitFullscreen();
+    }
+  },
+  false
+);
