54b8f5950a58b8190491eb44eab03cdb5fc2a32c
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-06-20 16:10:27 +0100

    Addded radial gradients

--- TheCanvas/src/gradients.js
+++ TheCanvas/src/gradients.js
@@ -24,3 +24,25 @@ ctx.beginPath();
 ctx.rect(0, 170, 85, 256);
 ctx.stroke();
 ctx.fill();
+
+/* Radial Gradients */
+/*
+let gradient = ctx.createRadialGradient(x0, y0, r0, x1, y1, r1);
+*/
+let radGrad = ctx.createRadialGradient(128, 213, 0, 128, 213, 42.5);
+
+radGrad.addColorStop(0, "rgba(255, 255, 255, 0.5)");
+radGrad.addColorStop(0.4999, "rgba(0, 0, 255, 1)");
+radGrad.addColorStop(0.5, "rgba(0, 0, 100, 1)");
+radGrad.addColorStop(1, "rgba(100, 0, 255, 0.5)");
+
+// define the styles
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 3;
+ctx.fillStyle = radGrad;
+
+//draw the shape
+ctx.beginPath();
+ctx.rect(85, 170, 85, 85); // xStart, yStart, xSize, ySize
+ctx.stroke();
+ctx.fill();
