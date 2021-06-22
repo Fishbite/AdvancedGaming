155b40b9ed2b2a2d81adb5dc325105005eef27b8
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-06-22 10:52:00 +0100

    Also added the globalAlpha() method to ratationAndScale. Didn't rename the file this time!!!

--- TheCanvas/src/rotationAndScale.js
+++ TheCanvas/src/rotationAndScale.js
@@ -1,23 +1,30 @@
 /* rotationAndScale.js */
 /*
     We decide to put the scale method in
-    here too, because just like rotation
+    here too, because just like rotation,
     scale needs to be placed between
     ctx.save(); and ctx.restore();
+
+    ctx.globalAlpha is also placed between
+    save and restore. It's another way of
+    making objects transparent!
 */
 
 //styles
 ctx.strokeStyle = "black";
 ctx.lineWidth = 1;
-ctx.fillStyle = "rgba(255, 200, 0, 0.25)";
+ctx.fillStyle = "rgba(255, 200, 0, 1)";
 
 /* !!!!!! IMOPRTANT !!!!! */
 // save the current state of the drawing // context, this will allow us to restore it
 // to its original state when we're finished
 ctx.save();
 
+// set global transparency:
+ctx.globalAlpha = 0.25;
+
 //move the drawing context's 0, 0, point
-// from the corner to the cneter of the
+// from the corner to the center of the
 // canvas.This will be the square center point:
 ctx.translate(128, 128);
 
@@ -27,7 +34,7 @@ ctx.rotate(45 * (Math.PI / 180));
 console.log("rads:", 45 * (Math.PI / 180));
 
 // Set the drawing context's scale method
-ctx.scale(0.75, 0.75);
+ctx.scale(1.4, 1.4); // Xscale, Yscale
 // draw a square with it's center at 0, 0
 ctx.beginPath();
 ctx.rect(-64, -64, 128, 128);
