b9a38bd05054954debbcb7eaad97cea88248dbd4
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-07-05 10:01:50 +0100

    We figured that the asset loader works correctly. WE now have to figure out how to use it!

--- sprites/src/assetsLoaderTest.js
+++ sprites/src/assetsLoaderTest.js
@@ -1,8 +1,8 @@
-import { assets_rex } from "../lib/assets_rex.js";
+import { assets } from "../lib/assets.js";
 import { makeCanvas } from "../lib/makeCanvas.js";
 
 // load the files
-assets_rex
+assets
   .load(["../images/animals.json", "../images/animals.png"])
   .then(() => setUp());
 
@@ -19,13 +19,19 @@ function setUp() {
   catImage.addEventListener("load", loadHandler, false);
   catImage.src = "../images/cat.png";
 
-  // try to use preloaded image fails with Tyoe error
+  let tigerImg = new Image();
+  console.log(tigerImg);
+  tigerImg.addEventListener("load", loadHandler, false);
+  tigerImg.src = "../images/tiger.png";
+
+  // try to use preloaded image fails with Type error
   //ctx.drawImage(assets["tiger.png"], 0, 0);
 
   // try another way of using preloaded image
   // also fails with the same Type error
-  let tiger = assets_rex["tiger.png"];
-  console.log(typeof tiger);
+  let tiger = new Image();
+  tiger.src = assets["tiger"];
+  console.log(tiger);
   let pattern = ctx.createPattern(tiger, "no-repeat");
   ctx.fillStyle = pattern;
   ctx.rect(0, 0, 64, 64);
@@ -34,7 +40,11 @@ function setUp() {
   //The loadHandler to draw the catIamage
   function loadHandler() {
     ctx.drawImage(catImage, 128, 128);
+    ctx.drawImage(tigerImg, 0, 128);
   }
+
+  let image = assets["tiger"];
+  ctx.drawImage(image, 0, 0);
 
   //render(canvas, ctx);
 }
