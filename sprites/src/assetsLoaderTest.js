a5b58f36faeb89d4fc2037dc4e7a65c822edbda3
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-07-08 00:04:27 +0100

    Working on it!

--- sprites/src/assetsLoaderTest.js
+++ sprites/src/assetsLoaderTest.js
@@ -3,7 +3,11 @@ import { makeCanvas } from "../lib/makeCanvas.js";
 
 // load the files
 assets
-  .load(["../images/animals.json", "../images/animals.png"])
+  .load([
+    "../images/animals.json",
+    "../images/animals.png",
+    "../images/tiger.png",
+  ])
   .then(() => setUp());
 
 function setUp() {
@@ -23,28 +27,58 @@ function setUp() {
   console.log(tigerImg);
   tigerImg.addEventListener("load", loadHandler, false);
   tigerImg.src = "../images/tiger.png";
-
-  // try to use preloaded image fails with Type error
-  //ctx.drawImage(assets["tiger.png"], 0, 0);
-
-  // try another way of using preloaded image
-  // also fails with the same Type error
-  let tiger = new Image();
-  tiger.src = assets["images/tiger.png"];
-  console.log(tiger);
-  let pattern = ctx.createPattern(tiger, "no-repeat");
-  ctx.fillStyle = pattern;
-  ctx.rect(0, 0, 64, 64);
-  ctx.fill();
-
   //The loadHandler to draw the catIamage
   function loadHandler() {
     ctx.drawImage(catImage, 128, 128);
     ctx.drawImage(tigerImg, 0, 128);
   }
 
-  let image = assets["tiger"];
-  ctx.drawImage(image, 0, 0);
+  class Sprite {
+    constructor(source, x = 0, y = 0) {
+      Object.assign(this, { x, y });
+
+      if (source instanceof Image) {
+        this.createFromImage(source);
+      }
+    }
+
+    createFromImage(source) {
+      if (!(source instanceof Image)) {
+        throw new Error(`${source} is not an image object`);
+      } else {
+        this.source = source;
+        this.sourceX = 0;
+        this.sourceY = 0;
+        this.sourceWidth = source.width;
+        this.sourceHeight = source.height;
+      }
+    }
+  }
+
+  //A higher-level wrapper
+  function sprite(source, x, y) {
+    let sprite = new Sprite(source, x, y);
+    stage.addChild(sprite);
+    return sprite;
+  }
+
+  let tiger = sprite(assets["images/tiger.png"], 0, 0);
+
+  // try to use preloaded image fails with Type error
+  //ctx.drawImage(assets["tiger.png"], 0, 0);
+
+  // try another way of using preloaded image
+  // also fails with the same Type error
+  // let tiger = new Image();
+  // tiger.src = assets["images/tiger.png"];
+  // console.log(tiger);
+  // let pattern = ctx.createPattern(tiger, "no-repeat");
+  // ctx.fillStyle = pattern;
+  // ctx.rect(0, 0, 64, 64);
+  // ctx.fill();
+
+  // let image = assets["tiger"];
+  // ctx.drawImage(image, 0, 0);
 
   //render(canvas, ctx);
 }
