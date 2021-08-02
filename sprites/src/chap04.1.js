5c02cfa076e45fe4368e425aa3430de633219a9f
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-08-02 11:37:42 +0100

    added additional methods to use images from various image source types

--- sprites/src/chap04.1.js
+++ sprites/src/chap04.1.js
@@ -769,22 +769,70 @@ function render(canvas) {
 
 class Sprite extends DisplayObject {
   constructor(source, x = 0, y = 0) {
-    // call the object's super
+    // call the displayObject's super
     super();
 
     // assign the argument values to this sprite
     Object.assign(this, { x, y });
 
+    /* ********* What is the source? ********* */
+    // figure out what the source is, then use that
+    // source data to display the sprite correctly
+
     // is the source a JS image object?
     if (source instanceof Image) {
       this.createFromImage(source);
     }
+
+    // If the source has a `frame` property the
+    // source is a tileset from a texture atlas
+    else if (source.frame) {
+      this.createFromAtlas(source);
+    }
+
+    // If the source contains an `image` subproperty, this must
+    // be a `frame` object that's defining the rectangular area
+    // of an inner subimage.
+    // Use that subimage to make the sprite. If it doesn't contain a
+    //`data` property, then it must be a single frame
+    else if (source.image && !source.data) {
+      this.createFromTileset(source);
+    }
+
+    // If the source contains an `image` subproperty and
+    // a `data` property, it contains multiple frames
+    else if (source.image & source.data) {
+      this.createFromTilesetFrames(source);
+    }
+
+    // Is the source an array? If so, what kind of array?
+    else if (source instanceof Array) {
+      if (source[0] && source[0].source) {
+        // the source is an array of frames on a texture
+        // atlas tileset
+        this.createFromAtlasFrames(source);
+      }
+
+      // it must be an array of image objects
+      else if (source[0] instanceof Image) {
+        this.createFromImages(source);
+      }
+
+      // throw an error if the sources in the array
+      // aren't reckognised
+      else {
+        throw new Error(`The image sourcees in ${source} are not reckognised`);
+      }
+    }
   }
 
   createFromImage(source) {
+    // throw an error if the source is not an Image object
     if (!(source instanceof Image)) {
       throw new Error(`${source} is not an image object`);
-    } else {
+    }
+    // otherwise, create the sprite using an image
+    else {
       this.source = source;
       this.sourceX = 0;
       this.sourceY = 0;
@@ -792,6 +840,115 @@ class Sprite extends DisplayObject {
       this.height = source.height;
       this.sourceWidth = source.width;
       this.sourceHeight = source.height;
+    }
+  }
+
+  createFromAtlas(source) {
+    this.tilesetFrame = source;
+    this.source = this.tilesetFrame.source;
+    this.sourceX = this.tilesetFrame.frame.x;
+    this.sourceY = this.tilesetFrame.frame.y;
+    this.width = this.tilesetFrame.frame.w;
+    this.height = this.tilesetFrame.frame.h;
+    this.sourceWidth = this.tilesetFrame.frame.w;
+    this.sourceHeight = this.tilesetFrame.frame.h;
+  }
+
+  createFromTileset(source) {
+    if (!(source.image instanceof Image)) {
+      throw new Error(`${source.image} is not an image object`);
+    } else {
+      this.source = source.image;
+      this.sourceX = source.x;
+      this.sourceY = source.y;
+      this.width = source.width;
+      this.height = source.height;
+    }
+  }
+
+  createFromTilesetFrames(source) {
+    if (!(source.image instanceof Image)) {
+      throw new Error(`${source.image} is not an Image object`);
+    } else {
+      this.source = source.image;
+      this.frames = source.data;
+
+      // set the sprite to the first frame
+      this.sourceX = this.frames[0][0];
+      this.sourceY = this.frames[0][1];
+      this.width = source.width;
+      this.height = source.height;
+      this.sourceWidth = source.width;
+      this.sourceHeight = source.height;
+    }
+  }
+
+  creatFromAtlasFrames(source) {
+    this.frames = source;
+    this.source = source[0].source;
+    this.sourceX = source[0].frame.x;
+    this.sourceY = source[0].frame.y;
+    this.width = source[0].frame.w;
+    this.height = source[0].frame.h;
+    this.sourceWidth = source[0].frame.w;
+    this.sourceHeight = source[0].frame.h;
+  }
+
+  createFromImages(source) {
+    this.frames = source;
+    this.source = source[0];
+    this.sourceX = 0;
+    this.sourceY = 0;
+    this.width = source[0].width;
+    this.height = source[0].height;
+    this.sourceWidth = soure[0].width;
+    this.sourceHeight = source[0].height;
+  }
+
+  // Add a `gotoAndStop` method to go to a specif frame
+  gotoAndStop(frameNumber) {
+    if (this.frames.length > 0 && frameNumber < this.frames.length) {
+      //a. Frames made from tileset subimages.
+      //If each frame is an array, then the frames were made from an
+      //ordinary Image object using the `frames` method
+      if (this.frames[0] instanceof Array) {
+        this.sourceX = this.frames[frameNumber][0];
+        this.sourceY = this.frames[frameNumber][1];
+      }
+
+      //b. Frames made from texture atlas frames.
+      //If each frame isn't an array, and it has a subobject called `frame`,
+      //then the frame must be a texture atlas ID name.
+      //In that case, get the source position from the atlas's `frame` object
+      else if (this.frames[frameNumber].frame) {
+        this.sourceX = this.frames[frameNumber].frame.x;
+        this.sourceY = this.frames[frameNumber].frame.y;
+        this.sourceWidth = this.frames[frameNumber].frame.w;
+        this.sourceHeight = this.frames[frameNumber].frame.h;
+        this.width = this.frames[frameNumber].frame.w;
+        this.height = this.frames[frameNumber].frame.h;
+      }
+
+      //c. Frames made from individual Image objects.
+      //If neither of the above is true, then each frame must be
+      //an individual Image object
+      else {
+        this.source = this.frames[frameNumber];
+        this.sourceX = 0;
+        this.sourceY = 0;
+        this.width = this.source.width;
+        this.height = this.source.height;
+        this.sourceWidth = this.source.width;
+        this.sourceHeight = this.source.height;
+      }
+
+      // set the `_currentFrame` value to the chosen frame
+      this._currentFrame = frameNumber;
+    }
+
+    // Throw an error if this sprite doesn't contain any frames
+    else {
+      throw new Error(`Frame number ${frameNumber} does not exist`);
     }
   }
 
@@ -837,6 +994,7 @@ function setup() {
   //   box.addChild(cat);
   console.log("box:", box);
 
+  // Making sprites from simgle images
   // let cat = sprite(assets["path"], posX, posY);
   let cat = sprite(assets["../images/cat.png"], 92, 92);
   cat.shadow = true;
