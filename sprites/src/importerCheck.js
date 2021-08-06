696c272eada48eb29634d330ae9cda0cea659396
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-08-06 21:20:35 +0100

    Created helper function to return radians from degrees

--- sprites/src/importerCheck.js
+++ sprites/src/importerCheck.js
@@ -39,7 +39,41 @@ function setup() {
   stage.width = canvas.width;
   stage.height = canvas.height;
 
-  // Make sprites here
+  // ****** Make sprites here ****** \\
+
+  // Use bluebox as a parent sprite
+  let blueBox = rectangle(
+    100,
+    100,
+    "RGB(0, 0, 255)",
+    "rgb(255, 100, 0)",
+    0,
+    100,
+    0
+  );
+  // Set the pivot points to the center of the sprite
+  blueBox.pivotX = 0.5;
+  blueBox.pivotY = 0.5;
+
+  // helper function to return radians from degrees
+  function deg(degrees) {
+    return degrees * (Math.PI / 180);
+  }
+  console.log(deg(45)); // 0.7853981633974483
+
+  // then we can rotate the sprite like this:
+  blueBox.rotation = deg(45); // rads = deg * (Math.PI / 180)
+  //blueBox.shadow = true;
+
+  // goldbox is the first child of bluebox
+  let goldbox = rectangle(50, 50, "gold");
+  blueBox.addChild(goldbox);
+  // Assign the goldbox's local coord's relative to bluebox
+  goldbox.x = 25;
+  goldbox.y = 25;
+
+  let xLine = line("black", 1, 0, 50, 300, 50);
+  let yLine = line("black", 1, 150, 0, 150, 150);
 
   // render the sprites on canvas
   render(canvas);
