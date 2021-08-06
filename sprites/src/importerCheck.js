0481d46899a4c281208c2606865996d95e405fd0
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-08-06 18:05:42 +0100

    setup function ready to add sprites

--- sprites/src/importerCheck.js
+++ sprites/src/importerCheck.js
@@ -18,16 +18,29 @@ import {
   frames,
 } from "../lib/importer.js";
 
-let canvas = makeCanvas(512, 512, "2px solid black", "#ffeeff");
-
 import { assets } from "../lib/assets.js";
 
-assets.load([
-  "../images/animals.json",
-  "../images/animals.png",
-  "../images/button.json",
-  "../images/cat.png",
-  "../images/fairy.png",
-  "../images/hedgehog.png",
-  "../images/tiger",
-]);
+assets
+  .load([
+    "../images/animals.json",
+    "../images/animals.png",
+    "../images/button.json",
+    "../images/cat.png",
+    "../images/fairy.png",
+    "../images/hedgehog.png",
+    "../images/tiger.png",
+  ])
+  .then(() => setup());
+
+function setup() {
+  console.log("setup OK!");
+  // Setup the canvas and stage
+  let canvas = makeCanvas(512, 512, "2px solid black", "#ffeeff");
+  stage.width = canvas.width;
+  stage.height = canvas.height;
+
+  // Make sprites here
+
+  // render the sprites on canvas
+  render(canvas);
+}
